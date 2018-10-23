0b0f04c2440bca18e2ce1abf3bf56e552495232d
Chng Shu Wen <shuwenchng96@gmail.com>
2018-10-23 13:43:45 +0800

    this is a first commit

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 27px;
+            width: 246px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p style="height: 22px; ">
+            hello world</p>
+        <p style="height: 22px; width: 180px">
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" ViewStateMode="Enabled"></asp:TextBox>
+        </p>
+        
+        <asp:TextBox ID="TextBox2" runat="server" Width="158px"></asp:TextBox>
+        
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="LOGIN" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int clickscount = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+               {
+                TextBox1.Text = "0";
+               
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //if(ViewState["Clicks"]!=null)
+            //{
+            //clickscount = (int)ViewState["Clicks"] + 1;
+            //}
+
+            //TextBox1.Text = clickscount.ToString();
+            //ViewState["Clicks"] = clickscount;
+            //clickscount = clickscount + 1;
+            //int clickscount = Convert.ToInt32(TextBox1.Text) + 1;
+            //TextBox1.Text = clickscount.ToString();
+
+
+
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Response.Write("The Text Has been Changed!");
+        }
+    }
+}
